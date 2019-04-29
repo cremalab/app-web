56b3cfc772414c64361c127656343a8e9b180ce1
Rob LaFeve <roblafeve@gmail.com>
2019-04-29 10:58:50 -0500

    Update makeReducer initialState

--- src/utils/makeReducer/index.ts
+++ src/utils/makeReducer/index.ts
@@ -7,7 +7,7 @@ export type MakeReducerCases<S, A extends Tagged> = (s: S) => Cases<A, S>
 export const makeReducer = <S, A extends Tagged>(
   makeReducerCases: MakeReducerCases<S, A>,
   initialState: S,
-): Reducer<S, A> => (state, action) => {
-  const cases = makeReducerCases(state || initialState)
-  return match<A, S>(cases, () => state || initialState)(action)
+): Reducer<S, A> => (state = initialState, action) => {
+  const cases = makeReducerCases(state)
+  return match<A, S>(cases, () => state)(action)
 }
