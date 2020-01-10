d3f7bdc37d05bb9832e53f379f3e08d5f1748584
Rob LaFeve <roblafeve@gmail.com>
2020-01-10 13:59:55 -0600

    CAW-7: Add ./src/types/README.md

--- src/types/Id.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Id - Type alias for `string`
- * ------------------------------------------------
- * Useful in the case where Id is represented
- * differently in the future
- */
-
-export type Id = string
