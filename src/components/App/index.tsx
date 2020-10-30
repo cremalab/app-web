a679a2ee1f98203307521936f7f5aeb5129e676d
Blake Tarter <blakertarter@gmail.com>
2020-10-30 15:38:10 -0500

    to-4: Update template to be in line with a fresh install of CRA, and add a little Crema flair

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -10,7 +10,7 @@ export function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/components/App/index.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
