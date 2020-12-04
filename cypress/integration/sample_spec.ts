6d6c267bf671a0fe0058114701208b899f8ba30d
Blake Tarter <blakertarter@gmail.com>
2020-12-04 10:47:52 -0600

    typescript-4: Updates to fix repo issues

--- cypress/integration/sample_spec.ts
+++ cypress/integration/sample_spec.ts
@@ -1,5 +1,7 @@
-describe("My First Test", function() {
-  it("Visits the Kitchen Sink", function() {
+describe("App", function () {
+  it("Contains a link to learn about React", function () {
     cy.visit("/")
+    cy.get(".App-link").contains("Learn React")
+    cy.get(".App-link").should("have.attr", "href", "https://reactjs.org")
   })
 })
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import { useState } from "react"
 import logo from "../../assets/logo.svg"
 import "./styles.css"
 
