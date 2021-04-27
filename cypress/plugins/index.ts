3a530f8d8b579614c619a18a62369bc121be1794
Rob LaFeve <roblafeve@gmail.com>
2021-04-27 09:46:49 -0500

    Add `@testing-library/cypress` (#744)
    * develop-707: Install @testing-library/cypress
    * develop-707: Configure @testing-library/cypress
    * develop-707: Change sample_spec to use findByText
    * develop-707: Improve cypress/plugins types

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -11,7 +11,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (_on, _config) => {
+export default (_on: Cypress.PluginEvents, _config: Cypress.ConfigOptions) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
