9c9d7d0559fe3fc56908214678e12e705f8e0f9e
Rob LaFeve <roblafeve@gmail.com>
2021-12-22 11:51:08 -0600

    Upgrade to react-scripts@5.0.0
    - Removes service worker file as this is no longer generated by default
    - Upgrades package-lock.json to version 2 (what latest npm uses)

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom"
 import { App } from "./components/App"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
-import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,11 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root"),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
