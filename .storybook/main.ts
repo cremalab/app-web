b4f0f3ef205d15105d56d1c8e86adf26ba614546
Rob LaFeve <roblafeve@gmail.com>
2022-03-07 09:19:59 -0600

    app-4: Fixes for storybook@6 and cra@5

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,8 @@
 module.exports = {
   stories: ['../src/**/?(*.)stories.tsx'],
-  addons: [
-    '@storybook/addon-actions', 
-    '@storybook/addon-essentials', 
-    '@storybook/addon-links', 
-    '@storybook/preset-create-react-app'
-  ],
+  addons: ['@storybook/addon-actions', '@storybook/addon-essentials', '@storybook/addon-links', '@storybook/preset-create-react-app'],
+  staticDirs: ['../public'],
+  core: {
+    builder: "webpack5"
+  }
 };
