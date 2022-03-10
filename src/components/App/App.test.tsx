d71931d2ce5fe3d9349559ec57ee46bf17eccfeb
Rob LaFeve <roblafeve@gmail.com>
2022-03-10 14:53:21 -0600

    Update App test and stories to match lint rules

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,10 +1,16 @@
-import ReactDOM from "react-dom"
+import { render, screen } from "@testing-library/react"
 import { App } from "./App"
 
 describe("App", () => {
-  it("renders", () => {
-    const div = document.createElement("div")
-    ReactDOM.render(<App />, div)
-    ReactDOM.unmountComponentAtNode(div)
+  it("renders expected text", () => {
+    // Arrange
+    const text = "Learn React"
+
+    // Act
+    render(<App />)
+    const received = screen.getByText(text)
+
+    // Assert
+    expect(received).toBeDefined()
   })
 })
