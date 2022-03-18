f5b323199478e88bf332d4bd270b468ca9c31376
Luiz Chagas <luizchagasjr@gmail.com>
2022-03-18 13:54:09 -0500

    Update App.test.tsx

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,15 +2,23 @@ import { render, screen } from "@testing-library/react"
 import { App } from "./App"
 
 describe("App", () => {
-  it("renders expected text", () => {
-    // Arrange
-    const text = "Learn React"
-
-    // Act
+  test("renders a banner with header element", () => {
     render(<App />)
-    const received = screen.getByText(text)
+    expect(screen.getByRole("banner")).toBeInTheDocument()
+  })
 
-    // Assert
-    expect(received).toBeDefined()
+  test("renders a logo with alt attribute", () => {
+    render(<App />)
+    expect(screen.getByRole("img")).toBeInTheDocument()
+    expect(screen.getByRole("img")).toHaveAttribute("alt", expect.any(String))
+    expect(screen.getByRole("img").getAttribute("alt")).not.toHaveLength(0)
+  })
+
+  test("renders a link that says 'Learn React'", () => {
+    render(<App />)
+    expect(screen.getByRole("link", { name: "Learn React" })).toHaveAttribute(
+      "href",
+      "https://reactjs.org",
+    )
   })
 })
